ff93b85b163afc39a6b86f3e6824268b357460c0
Mikeias <mikeias66@gmail.com>
2017-09-07 15:34:26 -0300

    Classification

---
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -0,0 +1,51 @@
+# Multiple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder = LabelEncoder()
+X[:, 3] = labelencoder.fit_transform(X[:, 3])
+onehotencoder = OneHotEncoder(categorical_features = [3])
+X = onehotencoder.fit_transform(X).toarray()
+
+# Avoiding the Dummy Variable Trap
+X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Fitting Multiple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+
+#Backward Elimination
+import statsmodels.formula.api as sm
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+
+columns_array = [0,1,2,3,4,5]
+significant_level = 0.05
+X_opt = X[:, columns_array]
+
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+pvalues = regressor_OLS.pvalues
+while pvalues.max() > significant_level:
+    max_index = np.where(pvalues==pvalues.max())[0][0]
+    del columns_array[max_index]
+    X_opt = X[:, columns_array]
+    regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+    pvalues = regressor_OLS.pvalues
+
